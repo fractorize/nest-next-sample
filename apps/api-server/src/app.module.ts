cb3973a406b10ad71340f48a249952cee4d90693
Manokaran Kittuswamy <manokaran@gmail.com>
2024-06-27 16:53:05 +0530

    Remove unwanted imports in app.module

--- apps/api-server/src/app.module.ts
+++ apps/api-server/src/app.module.ts
@@ -1,20 +1,17 @@
 import configuration from '../config/configuration';
-import { ConfigModule, ConfigService } from '@nestjs/config'; //Should always be the first import
+import { ConfigModule } from '@nestjs/config'; //Should always be the first import
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { Enhancer, GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { EmployeeModule } from './modules/employee/employee.module';
 import { EmployeeModule } from './graphql/employee/employee.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthGuard } from './auth/auth.guard';
-import { UserModule } from './user/user.module';
-import { Enhancer, GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { RootQuery } from './root.query';
-import { JwtService } from '@nestjs/jwt';
-import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -30,13 +27,11 @@ import { APP_GUARD } from '@nestjs/core';
     }),
     EmployeeModule,
     AuthModule,
-    UserModule,
   ],
   controllers: [AppController, AuthController],
   providers: [
     AppService,
     RootQuery,
-    JwtService,
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
