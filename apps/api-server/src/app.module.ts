a187afaa6a0a03ef459e1e5fc75f15807b82a31c
Manokaran Kittuswamy <manokaran@gmail.com>
2024-06-29 18:04:20 +0530

    Sample cron job added

--- apps/api-server/src/app.module.ts
+++ apps/api-server/src/app.module.ts
@@ -1,32 +1,35 @@
-import configuration from '../config/configuration';
-import { ConfigModule } from '@nestjs/config'; //Should always be the first import
-import { join } from 'path';
-import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
-import { Enhancer, GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { RootQuery } from './root.query';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { AuthController } from './auth/auth.controller';
-import { AuthGuard } from './auth/auth.guard';
-import { AccessModule } from './access/access.module';
-import { AccessGuard } from './access/access.guard';
+import configuration from "../config/configuration";
+import { ConfigModule } from "@nestjs/config"; //Should always be the first import
+import { join } from "path";
+import { Module } from "@nestjs/common";
+import { APP_GUARD } from "@nestjs/core";
+import { Enhancer, GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { RootQuery } from "./root.query";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { AuthController } from "./auth/auth.controller";
+import { AuthGuard } from "./auth/auth.guard";
+import { AccessModule } from "./access/access.module";
+import { AccessGuard } from "./access/access.guard";
 // import { EmployeeModule } from './graphql/employee/employee.module';
-import { EmployeeModule } from './employee/employee.module';
-import { EmployeeController } from './employee/employee.controller';
+import { EmployeeModule } from "./employee/employee.module";
+import { EmployeeController } from "./employee/employee.controller";
+import { ScheduleModule } from "@nestjs/schedule";
+import { TasksService } from "./scheduled-tasks/tasks.service";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['../.env.local'],
+      envFilePath: ["../.env.local"],
       load: [configuration],
     }),
+    ScheduleModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+      autoSchemaFile: join(process.cwd(), "src/graphql/schema.gql"),
       sortSchema: true,
     }),
     AuthModule,
@@ -44,7 +47,8 @@ import { EmployeeController } from './employee/employee.controller';
     {
       provide: APP_GUARD,
       useClass: AccessGuard,
-    }
+    },
+    TasksService,
   ],
 })
 export class AppModule {}
