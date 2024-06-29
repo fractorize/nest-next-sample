8b04eccc16b09858b22d06c0b34d9bfff21eecbd
Manokaran Kittuswamy <manokaran@gmail.com>
2024-06-29 17:33:13 +0530

    Add Logger to email service

--- apps/messaging/src/email/email.service.ts
+++ apps/messaging/src/email/email.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { MailerService } from "@nestjs-modules/mailer";
 import { Email } from "src/types/email";
 
 @Injectable()
 export class EmailService {
+  private readonly logger = new Logger(EmailService.name);
   constructor(private readonly mailService: MailerService) {}
 
-  async sendEmail(data: Email) {
-    const result = await this.mailService.sendMail(data);
-    return result;
+  async sendEmail(email: Email) {
+    try {
+      const result = await this.mailService.sendMail(email);
+      this.logger.log(`Email sent successfully to ${email.to}`);
+      return !!result;
+    } catch (error) {
+      this.logger.error(`Error sending email to ${email.to}`);
+      throw error;
+    }
   }
 }
