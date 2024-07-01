6014c75db64ad88d7d8feeb65685768629edc1dd
Manokaran Kittuswamy <manokaran@gmail.com>
2024-07-01 18:13:23 +0530

    Enable messaging in api-server based on .env.local setting

--- apps/api-server/src/queues/producer.service.ts
+++ apps/api-server/src/queues/producer.service.ts
@@ -5,18 +5,29 @@ import {
   Injectable,
   Logger,
 } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { ClientProxy } from "@nestjs/microservices";
 import { Email } from "../types/email";
 
 @Injectable()
 export class ProducerService {
-  constructor(@Inject("EMAIL_QUEUE") private client: ClientProxy) {
-    this.client.connect();
+  private messagingEnabled: boolean;
+  constructor(
+    @Inject("EMAIL_QUEUE") private client: ClientProxy,
+    private configService: ConfigService,
+  ) {
+    this.messagingEnabled =
+      this.configService.get("ENABLE_MESSAGING") === "true";
+    if (this.messagingEnabled) {
+      this.client.connect();
+    }
   }
 
   addToEmailQueue(mail: Email) {
     try {
-      this.client.emit("sendEmail", mail);
+      if (this.messagingEnabled) {
+        this.client.emit("sendEmail", mail);
+      }
     } catch (error) {
       console.log(error);
       throw new HttpException(
